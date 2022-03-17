dc5f802ca783eaa8a43535a91f24792eba1b4723
Agwen <pawel.pycia29@gmail.com>
2022-03-17 22:10:12 +0100

    skończyłem to gównoooooooooooooooogit add .git add .git add .git add .git add .! AAAAAAAAAAAAAAA

---
+++ lab1/zad3/zad3b/wclibrary.h
@@ -0,0 +1,21 @@
+//
+// Created by agwen on 15.03.2022.
+//
+
+#ifndef LAB1_EX01_H
+#define LAB1_EX01_H
+#include <stdio.h>
+
+struct block {
+    char* arr;
+    long size;
+};
+int find_empty_index();
+long get_file_size(FILE *fd);
+void wc_command(char** files, int number_of_file);
+void create_block_table(int number_of_blocks);
+void remove_block(int index);
+int reserve_block();
+void print_block(int index);
+void free_table();
+#endif //LAB1_EX01_H
